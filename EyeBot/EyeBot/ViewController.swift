361576043e341512fbecf88ebf93da39eac880ab
Chris Sansavera <chris.sansavera@siemens.com>
2017-05-12 13:44:01 -0400

    Start Camera Functionality

--- EyeBot/EyeBot/ViewController.swift
+++ EyeBot/EyeBot/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  EyeBot
@@ -8,18 +9,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        actionLaunchCamera()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func actionLaunchCamera()
+    {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        imagePicker.allowsEditing = true
+            
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
