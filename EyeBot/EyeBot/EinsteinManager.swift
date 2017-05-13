c2668fd0a193072f225d5c84a81ce8cf9013f6cb
Luis Padron <luis@luispadron.com>
2017-05-13 16:05:16 -0400

    Clean out pods

--- EyeBot/EyeBot/EinsteinManager.swift
+++ EyeBot/EyeBot/EinsteinManager.swift
@@ -8,23 +8,22 @@
 
 import Foundation
 import Alamofire
-import SalesforceEinsteinVision
 
 /// Prediction struct, returned in a prediction call when using EinsteinManager
 public struct Prediction {
-    let mostProbable: Probability
-    let leastProbable: Probability
-    let allProbabilities: [Probability]
-    
-    init(probabilities: [Probability]) {
-        
-        let probs = probabilities.sorted {
-            $0.probability ?? 0 > $1.probability ?? 0
-        }
-        self.mostProbable = probs.first!
-        self.leastProbable = probs.last!
-        self.allProbabilities = probabilities
-    }
+//    let mostProbable: Probability
+//    let leastProbable: Probability
+//    let allProbabilities: [Probability]
+//    
+//    init(probabilities: [Probability]) {
+//        
+//        let probs = probabilities.sorted {
+//            $0.probability ?? 0 > $1.probability ?? 0
+//        }
+//        self.mostProbable = probs.first!
+//        self.leastProbable = probs.last!
+//        self.allProbabilities = probabilities
+//    }
 }
 
 /// The EinsteinManager handles all the API calls to the Einstein image prediction service
@@ -43,39 +42,15 @@ open class EinsteinManager {
     open var token: String?
     
     /// Typealias for the completion of a prediction call
-    public typealias PredictionCompletion = (Prediction?) -> Void
+    public typealias PredictionCompletion = (Prediction) -> Void
     
-    /// The prediction service for the Einstein manager
-    private lazy var service: PredictionService = {
-        guard let token = self.token else {
-            fatalError("Token not set for Manager: \(self)")
-        }
-        
-        return PredictionService(bearerToken: token)!
-    }()
     
-    public func predictImage(_ img: UIImage, withModelId modelId: String, completion: @escaping PredictionCompletion) {
+    public func predictImage(_ img: UIImage, withModelId modelId: String, completion: PredictionCompletion) {
         guard let imgData = UIImagePNGRepresentation(img) else {
             fatalError("Error converting image to png representation: \(img)")
         }
         
         let base64Img = imgData.base64EncodedString()
         
-        service.predictBase64(modelId: modelId,
-                              base64: base64Img,
-                              sampleId: "")
-        { [unowned self] (result) in
-            let prediction = self.collectResult(result)
-            completion(prediction)
-        }
-    }
-    
-    private func collectResult(_ result: PredictionResult?) -> Prediction? {
-        guard let probs = result?.probabilities else {
-            print("No results collected")
-            return nil
-        }
-        
-        return Prediction(probabilities: probs)
     }
 }
