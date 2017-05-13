a243218f8fca416f58094f7e7945947b1a755a0e
Luis Padron <luis@luispadron.com>
2017-05-13 17:48:28 -0400

    Add prediction of single image

--- EyeBot/EyeBot/AppDelegate.swift
+++ EyeBot/EyeBot/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         EinsteinManager.shared.token = "cee371765729d48718c4ded34eb3fa44a9ad41b7"
+        
         return true
     }
 
--- EyeBot/EyeBot/EinsteinManager.swift
+++ EyeBot/EyeBot/EinsteinManager.swift
@@ -9,21 +9,41 @@
 import Foundation
 import Alamofire
 
+/// The probability for a predicted image
+public struct Probability {
+    let label: String
+    let percent: Double
+    
+    init(label: String, percent: Double) {
+        self.label = label
+        self.percent = percent
+    }
+}
+
 /// Prediction struct, returned in a prediction call when using EinsteinManager
 public struct Prediction {
-//    let mostProbable: Probability
-//    let leastProbable: Probability
-//    let allProbabilities: [Probability]
-//    
-//    init(probabilities: [Probability]) {
-//        
-//        let probs = probabilities.sorted {
-//            $0.probability ?? 0 > $1.probability ?? 0
-//        }
-//        self.mostProbable = probs.first!
-//        self.leastProbable = probs.last!
-//        self.allProbabilities = probabilities
-//    }
+    let mostProbable: Probability
+    let leastProbable: Probability
+    let allProbabilities: [Probability]
+
+    init?(withJSON json: [String : Any]) {
+        guard let probabiltiesJSON = json["probabilities"] as? [AnyObject] else {
+            return nil
+        }
+        
+        var probabilities = [Probability]()
+        
+        for probJSON in probabiltiesJSON {
+            let label = probJSON.value(forKey: "label") as! String
+            let percent = probJSON.value(forKey: "probability") as! Double
+            probabilities.append(Probability(label: label, percent: percent * 100))
+        }
+        
+        probabilities.sort { $0.percent > $1.percent }
+        self.mostProbable = probabilities.first!
+        self.leastProbable = probabilities.last!
+        self.allProbabilities = probabilities
+    }
 }
 
 /// The EinsteinManager handles all the API calls to the Einstein image prediction service
@@ -33,24 +53,81 @@ open class EinsteinManager {
     open static let shared = EinsteinManager()
     
     /// General Image ID for classifying
-    static let generalImageId = "GeneralImageClassifier"
+    public static let generalImageId = "GeneralImageClassifier"
+    
+    /// The header for the post request when predicting an image
+    public var predictHeaders: HTTPHeaders = [
+        "Cache-Control" : "no-cache",
+        "Content-Type"  : "multipart/form-data"
+    ]
     
     /// The API Url endpoint which the manager will interact with
     open var apiUrl: String = "https://api.metamind.io/v1/vision/predict/"
     
     /// The JWT token that is grabbed from the Einstein authorization page
-    open var token: String?
+    open var token: String? {
+        didSet {
+            // Update the predict header to include the authorization
+            if let token = self.token {
+                self.predictHeaders["Authorization"] = "Bearer \(token)"
+            }
+        }
+    }
     
     /// Typealias for the completion of a prediction call
-    public typealias PredictionCompletion = (Prediction) -> Void
+    public typealias PredictionCompletion = (Prediction?) -> Void
     
     
-    public func predictImage(_ img: UIImage, withModelId modelId: String, completion: PredictionCompletion) {
+    public func predictImage(_ img: UIImage, withModelId modelId: String, completion: @escaping PredictionCompletion) {
         guard let imgData = UIImagePNGRepresentation(img) else {
             fatalError("Error converting image to png representation: \(img)")
         }
         
-        let base64Img = imgData.base64EncodedString()
+        let base64String = imgData.base64EncodedString()
+        guard let base64ImgData = base64String.data(using: .utf8, allowLossyConversion: false),
+            let modelIdData = modelId.data(using: .utf8, allowLossyConversion: false)
+        else {
+            fatalError("Error converting either modelID or img to to data")
+        }
         
+        Alamofire.upload(
+            multipartFormData:
+            { (multipartFormData) in
+                // Create the form data and append it
+                multipartFormData.append(base64ImgData,
+                                         withName: "sampleBase64Content",
+                                         mimeType: "text/plain")
+                multipartFormData.append(modelIdData,
+                                         withName: "modelId",
+                                         mimeType: "text/plain")
+            },
+            usingThreshold: UInt64(),
+            to: self.apiUrl,
+            method: .post,
+            headers: self.predictHeaders)
+            { (uploadResult) in
+                self.collectJSON(fromResult: uploadResult, completion: completion)
+            }
+    }
+    
+    private func collectJSON(fromResult result: SessionManager.MultipartFormDataEncodingResult,
+                             completion: PredictionCompletion?) {
+        
+        switch result {
+
+        case .success(let response, _, _):
+            response.responseJSON
+            { (jsonResponse) in
+                guard let json = jsonResponse.result.value as? [String : Any] else {
+                    print("Received no JSON which is convertable to [String: Any] in prediction response")
+                    return
+                }
+                
+                 completion?(Prediction(withJSON: json))
+            }
+            
+        case .failure(let error):
+            print("Error collecting result from prediction, error: \(error)")
+        }
     }
 }
