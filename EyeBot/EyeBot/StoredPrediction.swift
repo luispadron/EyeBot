96d16d4a71e562151cecb357b28f1a4cb5a0b199
Luis Padron <luis@luispadron.com>
2017-05-15 09:57:40 -0400

    Add skeleton prediction model

---
+++ EyeBot/EyeBot/StoredPrediction.swift
@@ -0,0 +1,13 @@
+//
+//  StoredPrediction.swift
+//  EyeBot
+//
+//  Created by Luis Padron on 5/15/17.
+//  Copyright © 2017 com.eyebot. All rights reserved.
+//
+
+import RealmSwift
+
+class StoredPrediction: Object {
+    
+}
