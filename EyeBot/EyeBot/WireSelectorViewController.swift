8993bfe37555806a68a334b6e43dd51ea4a7d668
perkins.christopher@siemens.com <perkins.christopher@siemens.com>
2017-05-16 10:30:37 -0400

    Added wireSelectorViewController skeleton

---
+++ EyeBot/EyeBot/WireSelectorViewController.swift
@@ -0,0 +1,12 @@
+//
+//  wireSelectorViewController.swift
+//  EyeBot
+//
+//  Created by christopher paul perkins on 5/16/17.
+//  Copyright © 2017 com.eyebot. All rights reserved.
+//
+
+import UIKit
+
+class WireSelectorViewController: UIViewController {
+}
